311dfc49a4fbb31b33073a3ae4f11f662cf6b5c1
Hero2838 <hmhero3@gmail.com>
2017-03-23 18:33:00 +0600

    Add files via upload
    Result system

---
+++ result.py
@@ -0,0 +1,15 @@
+marks = int(input("Enter your marks: "))
+if marks >= 80:
+	print("Your grade is A+")
+elif marks >= 79:
+	print("Your grade is A")
+elif marks >= 69:
+	print("Your grade is A-")
+elif marks >= 59:
+	print("Your grade is B")
+elif marks >= 49:
+	print("Your grade is C")
+elif marks >= 40:
+	print("Your grade is D")
+else:
+	print("Failed")
